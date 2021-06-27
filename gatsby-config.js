0bf0c18fb54a05f6ac867f3bd66957626a66ae74
Dervis Selimovic <dervis.selimovic@moonvision.io>
2021-06-28 00:48:41 +0200

    delete commented out code from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,6 @@ require("dotenv").config({
 
 module.exports = {
   plugins: [
-    // `gatsby-transformer-yaml`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     path: `./resume`,
-    //   },
-    // },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-datocms`,
